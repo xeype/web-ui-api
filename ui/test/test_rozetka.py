6609d553b5bccfc05c0e094ce5ba64a0b170ff48
xeype <alexeypedko@gmail.com>
2022-01-10 14:01:36 +0200

    add new tests

--- ui/test/test_rozetka.py
+++ ui/test/test_rozetka.py
@@ -1,4 +1,4 @@
-from ui.pages.main_page import SearchHelper
+from ui.pages.main_page import MainPageHelper
 from ui.pages.all_products_page import AllProductsPageHelper
 from ui.pages.product_page import ProductPageHelper
 from ui.pages.cart_page import CartPageHelper
@@ -6,7 +6,7 @@ import pytest
 
 
 def test_check_cart_is_empty_by_default(browser):
-    main_rozetka = SearchHelper(browser)
+    main_rozetka = MainPageHelper(browser)
     cart_page = CartPageHelper(browser)
 
     main_rozetka.go_to_site()
@@ -17,7 +17,7 @@ def test_check_cart_is_empty_by_default(browser):
 
 @pytest.mark.parametrize('product', ['ВИНО', 'сыр', 'Пиво'])
 def test_find_product(browser, product):
-    main_rozetka = SearchHelper(browser)
+    main_rozetka = MainPageHelper(browser)
     products_page = AllProductsPageHelper(browser)
 
     main_rozetka.go_to_site()
@@ -30,7 +30,7 @@ def test_find_product(browser, product):
 
 @pytest.mark.parametrize('product', ['чай', 'Кофе'])
 def test_cart_pop_up_is_showed_after_add_product(browser, product):
-    main_rozetka = SearchHelper(browser)
+    main_rozetka = MainPageHelper(browser)
     products_page = AllProductsPageHelper(browser)
     product_page = ProductPageHelper(browser)
     cart_page = CartPageHelper(browser)
@@ -49,3 +49,19 @@ def test_cart_pop_up_is_showed_after_add_product(browser, product):
 
     assert cart_page.get_cart_title() == 'Корзина'
     assert product_title.lower() in str(cart_product_list).lower()
+
+
+@pytest.mark.parametrize('category', ['автотовары', 'книги', 'смартфон'])
+def test_go_to_products_page_by_categories(browser, category):
+    main_rozetka = MainPageHelper(browser)
+    products_page = AllProductsPageHelper(browser)
+
+    browser.maximize_window()
+
+    main_rozetka.go_to_site()
+
+    main_rozetka.click_on_the_category_by_name(category)
+
+    category_title = products_page.get_category_name()
+
+    assert category.lower() in category_title.lower()
