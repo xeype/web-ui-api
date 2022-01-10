6a282054a525e0adc39a4c56a46d0087edf0966d
xeype <alexeypedko@gmail.com>
2022-01-10 18:41:38 +0200

    add new fixture

--- ui/test/conftest.py
+++ ui/test/conftest.py
@@ -7,3 +7,16 @@ def browser():
     driver = webdriver.Chrome()
     yield driver
     driver.quit()
+
+
+default_redirect_categories = ['Ноутбуки и компьютеры', 'Смартфоны, ТВ и электроника', 'Товары для геймеров',
+                               'Бытовая техника', 'Товары для дома', 'Инструменты и автотовары',
+                               'Сантехника и ремонт', 'Дача, сад и огород', 'Спорт и увлечения',
+                               'Красота и здоровье', 'Детские товары',
+                               'Зоотовары', 'Канцтовары и книги', 'Алкогольные напитки и продукты',
+                               'Товары для бизнеса и услуги']
+
+
+@pytest.fixture(params='default_redirect_categories')
+def categories(request):
+    return request.param
